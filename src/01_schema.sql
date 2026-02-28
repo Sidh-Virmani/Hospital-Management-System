87db4adb5b545404ab6e366e44169b726f588078
Rhea Jain <rheacswork@gmail.com>
2026-02-28 18:57:09 +0530

    Added nurse, doctor table, edited medical staff table

--- src/01_schema.sql
+++ src/01_schema.sql
@@ -13,9 +13,9 @@ CREATE TABLE users (
 );
 
 CREATE TABLE medical_staff(
-    staff_id INT PRIMARY KEY AUTO INCREMENT,
-    user_id INT NOT NULL,
-    staff_type ENUM("DOCTOR", "NURSE") NOT NULL,
+    staff_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL UNIQUE,
+    staff_type ENUM('DOCTOR', 'NURSE') NOT NULL,
     department_id INT,
 
     constraint fk_medical_staff_user
@@ -23,4 +23,29 @@ CREATE TABLE medical_staff(
         REFERENCES users(user_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-)
+);
+
+CREATE TABLE doctors(
+    doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL UNIQUE,
+    specialization VARCHAR(100),
+    availability_status ENUM('BUSY', 'AVAILABLE', 'ON LEAVE', 'OFF DUTY') DEFAULT 'AVAILABLE' NOT NULL,
+
+    constraint fk_doctor_staff
+        FOREIGN KEY (staff_id)
+        REFERENCES medical_staff(staff_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE nurses(
+    nurse_id INT PRIMARY KEY AUTO_INCREMENT,
+    staff_id INT NOT NULL UNIQUE,
+    ward_assigned VARCHAR(100),
+
+    constraint fk_nurse_staff
+        FOREIGN KEY (staff_id)
+        REFERENCES medical_staff(staff_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
